93cddcccbb6b46c7ce738c8f2bc2e7a59a2a8634
Saptarshi Chatterjee <saptarshi06chatterjee@gmail.com>
2026-06-12 09:41:36 +0530

    Make coinId, liveInterval, and setLiveInterval optional

--- type.d.ts
+++ type.d.ts
@@ -8,13 +8,13 @@ interface NextPageProps {
 interface CandlestickChartProps {
     data?: OHLCData[];
     liveOhlcv?: OHLCData | null;
-    coinId: string;
+    coinId?: string;
     height?: number;
     children?: React.ReactNode;
     mode?: 'historical' | 'live';
     initialPeriod?: Period;
-    liveInterval: '1s' | '1m';
-    setLiveInterval: (interval: '1s' | '1m') => void;
+    liveInterval?: '1s' | '1m';
+    setLiveInterval?: (interval: '1s' | '1m') => void;
 }
 
 interface ConverterProps {
